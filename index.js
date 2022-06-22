6f5721f3828be46e4193ca5961bc1321ee577360
iRed <94511889+iRed-Github@users.noreply.github.com>
2022-06-22 11:58:22 +0530

    Added via Upload

---
+++ index.js
@@ -0,0 +1,103 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const { Client, Collection, Intents, MessageEmbed } = require("discord.js");
+const { DEFAULT_PREFIX, BOT_TOKEN, ERROR_LOGS_CHANNEL, YT_COOKIE } = require("./config.json");
+const { loadCommands } = require("./handler/loadCommands");
+const { loadEvents } = require("./handler/loadEvents");
+const { loadSlashCommands } = require("./handler/loadSlashCommands")
+const { loadPlayerEvents } = require("./handler/loadPlayerEvents");
+const { DiscordTogether } = require('discord-together');
+const { Player } = require('discord-player');
+const Enmap = require("enmap");
+
+const client = new Client({
+  allowedMentions: { parse: ["users", "roles"] },
+  intents: 32767
+});
+const { checkValid } = require("./functions/validation/checkValid");
+const Embeds = require("./functions/embeds/Embeds");
+const Logger = require("./functions/Logger/Logger");
+const Util = require("./functions/util/Util");
+
+client.discordTogether = new DiscordTogether(client);
+client.commands = new Collection();
+client.slash = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./Commands/");
+client.setMaxListeners(0);
+const Cookie = YT_COOKIE;
+client.logger = Logger;
+client.utils = Util;
+client.say = Embeds;
+const player = new Player(client, {
+  leaveOnEnd: true,
+  leaveOnStop: false,
+  leaveOnEmpty: false,
+  leaveOnEmptyCooldown: 6000000,
+  autoSelfDeaf: true,
+  initialVolume: 100,
+  ytdlDownloadOptions: {
+    requestOptions: {
+      headers: {
+        cookie: Cookie,
+      }
+    }
+  },
+});
+
+client.player = player;
+client.db = new Enmap({ name: "musicdb" });
+
+loadCommands(client);
+loadEvents(client);
+loadPlayerEvents(client);
+loadSlashCommands(client);
+checkValid();
+
+// Error Handling
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception: " + err);
+
+  const exceptionembed = new MessageEmbed()
+  .setTitle("Uncaught Exception")
+  .setDescription(`${err}`)
+  .setColor("RED")
+  client.channels.cache.get(ERROR_LOGS_CHANNEL).send({ embeds: [exceptionembed] })
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log(
+    "[FATAL] Possibly Unhandled Rejection at: Promise ",
+    promise,
+    " reason: ",
+    reason.message
+  );
+
+   const rejectionembed = new MessageEmbed()
+  .setTitle("Unhandled Promise Rejection")
+  .addField("Promise", `${promise}`)
+  .addField("Reason", `${reason.message}`)
+  .setColor("RED")
+  client.channels.cache.get(ERROR_LOGS_CHANNEL).send({ embeds: [rejectionembed] })
+});
+
+client.login(BOT_TOKEN || process.env.TOKEN).then(() => {
+  console.log(
+    chalk.bgBlueBright.black(
+      ` Successfully logged in as: ${client.user.tag}`
+    )
+  );
+});
+// if your on a hosting platform where env's can be used to hide stuff then put your discord bot token in env
+// check example.env file for some info
+/**
+ * @INFO
+ * Bot Coded by iRed#1330 | https://github.com/iRed-Github/Chronium-BOT
+ * @INFO
+ * Join iDK Development | https://dsc.gg/idk-development
+ * @INFO
+ * Please mention Her / iDK Development, when using this Code!
+ * @INFO
+ */
