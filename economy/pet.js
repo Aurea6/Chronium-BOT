0365560acb61620e68d9da866a21db7f9ab485d8
iRed <94511889+iRed-Github@users.noreply.github.com>
2022-07-21 18:16:30 +0530

    Add files via upload

---
+++ economy/pet.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'Kraken',
+    emoji: "<:kraken:990491577468207155>",
+price: "30000",
+    id: 'kraken'
+  },
+  {
+    name: 'chimken',
+    emoji: "<:chimken:990492254424686633>",
+price: "40000",
+    id: 'chimken'
+  },
+  {
+    name: 'catgirl',
+    emoji: "<:catgirl:990492635238137857>",
+price: "60000",
+    id: 'catgirl'
+  }
+  
+  
+]
---
+++ economy/shop.js
@@ -0,0 +1,154 @@
+module.exports = [
+  {
+    items: "banknote",
+    price: {buy: 50000, sell: 0},
+    desc: "Use this note to expand your bank space",
+    emoji: "💵",
+    type: "tools",
+    rarity: "legendary"
+  },
+  {
+    items: "camera",
+    price: {buy: 5000, sell: 0},
+    desc: "Use this to take pictute and earn money",
+    emoji: "📷",
+    type: "tools",
+    rarity: "rare"
+  },
+  {
+    items: "blob",
+    price: {buy: 0, sell: 0},
+    desc: "The Rarest and Craziest item",
+    emoji: "<a:blob:990481613093765120>",
+    type: "collectables",
+    rarity: "omega"
+  },
+  {
+    items: "laptop",
+    price: {buy: 7100, sell: 500},
+    desc: "Use laptop to post memes and get money!",
+    emoji: "💻",
+    type: "tools",
+    rarity: "rare"
+  },
+  {
+    items: "crown",
+    price: {buy: 70000000, sell: 0},
+    desc: "Only the richest player have this item",
+    emoji: "👑",
+    type: "collectables",
+    rarity: "omega"
+  },
+  {
+    items: "padlock",
+    price: {buy: 5000, sell: 2300},
+    desc: "Lock your wallet so no one can rob you!",
+    emoji: "🔒",
+    type: "tools",
+    rarity: "rare"
+  },
+  {
+    items: "fishingrod",
+    price: {buy: 6750, sell: 1200},
+    desc: "Use fishing rods to go fishing and get some money!!",
+    emoji: "🎣",
+    type: "tools",
+    rarity: "uncommon"
+  },
+  {
+    items: "huntingrod",
+    price: {buy: 6750, sell: 1200},
+    desc: "Use hunting rods to go hunting and get some money!!",
+    emoji: "🔫",
+    type: "tools",
+    rarity: "uncommon"
+  },
+  {
+    items: 'commonfish',
+    price: {buy: 0, sell: 1800},
+    desc: 'collect fishes and sell them to get money',
+    emoji: '🐟',
+    type: 'sellable',
+    rarity: 'common'
+  },
+  {
+    items: 'rarefish',
+    price: {buy: 0, sell: 6000},
+    desc: 'collect fishes and sell them to get money',
+    emoji: '🐠',
+    type: 'sellable',
+    rarity: 'rare'
+  },
+  {
+    items: 'exoticfish',
+    price: {buy: 0, sell: 6900},
+    desc: 'collect fishes and sell them to get money',
+    emoji: '🐋',
+    type: 'sellable',
+    rarity: 'ultra rare'
+  },
+  {
+    items: 'ladybug',
+    price: {buy: 0, sell: 1800},
+    desc: 'collect animals and sell them to get money',
+    emoji: '🐞',
+    type: 'sellable',
+    rarity: 'rare'
+  },
+  {
+    items: 'bunny',
+    price: {buy: 0, sell: 6000},
+    desc: 'collect animals and sell them to get money',
+    emoji: '🐇',
+    type: 'sellable',
+    rarity: 'ultra rare'
+  },
+  {
+    items: 'deer',
+    price: {buy: 0, sell: 6900},
+    desc: 'collect animals and sell them to get money',
+    emoji: '🦌',
+    type: 'sellable',
+    rarity: 'ultra rare'
+  },
+  {
+    items: 'santasbag',
+    price: {buy: 0, sell: 0},
+    desc: 'Use this to get some items and caah',
+    emoji: '<:Santa_Bag:990490416946237532>',
+    type: 'lootbox',
+    rarity: 'ultra rare'
+  },
+  {
+    items: 'spookybox',
+    price: {buy: 1000000, sell: 0},
+    desc: 'I wonder what this does hmmm',
+    emoji: '<:spooky_box:990490618901962802>',
+    type: 'lootbox',
+    rarity: 'ultra rare'
+  },
+    {
+    items: 'spinner',
+    price: {buy: 6000, sell: 0},
+    desc: 'use this to get multi in gamble',
+    emoji: '<a:FidgetSpinner:990490943805341736>',
+    type: 'useable',
+    rarity: 'rare'
+  },
+      {
+    items: 'xmaspresent',
+    price: {buy: 500000, sell: 0},
+    desc: 'use this to get random amount of snowballs or cash',
+    emoji: '🎁',
+    type: 'lootbox',
+    rarity: 'legendary'
+      },
+  {
+    items: 'snowball',
+    price: {buy: 0, sell: 1000},
+    desc: 'Limited Christmas 2021 item',
+    emoji: '❄️',
+    type: 'sellable',
+    rarity: 'legendary'
+  }
+]
