efcde66b5a9349e9dc4d599dfb347cfe6a48d5ba
iRed <94511889+iRed-Github@users.noreply.github.com>
2022-07-21 17:50:20 +0530

    Added things | v1.0.3 version stuff
    Added how to make a command in this code
    Added how a command template
    Added emojis config

---
+++ command-template.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+module.exports = {
+   name: "",
+   aliases: ["", "", ""],
+   cooldowns: 3000,
+   description: "",
+   usage: "",
+   toggleOff: false,
+   developersOnly: false,
+   userpermissions: ["SEND_MESSAGES", "VIEW_CHANNEL"],
+   botpermissions: ["ADMINISTRATOR"],
+
+   run: async (client, message, args) => {},
+};
+/**
+ * @INFO
+ * Bot Coded by iRed#1330 | https://github.com/iRed-Github/Chronium-BOT
+ * @INFO
+ * Join iDK Development | https://dsc.gg/idk-development
+ * @INFO
+ * Please mention Her / iDK Development, when using this Code!
+ * @INFO
+ */
